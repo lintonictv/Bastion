ce2c8192a488ae579a8535ad52bfc6cd22f25697
lintonic <>
2018-05-21 21:08:12 -0700

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () =>
+ <div style={{ color: `teal` }}>
+   <h1>Introducing Project Bastion</h1>
+   <p>Welcome to the Bastion Network</p>
+   <p>You have been drafted by our executives to embark on a special mission</p>
+   <img src="https://i.imgur.com/i9zL8G9.jpg" alt="" />
+ </div>
